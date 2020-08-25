cc6eae91d3a0e6180a96bdc15107dbe6e26510b4
longmt0225 <longmt.k8.changshan21@gmail.com>
2020-08-25 18:56:09 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-	def index	# indexアクションを定義した
-		@post = Post.find(1)	# 1番目のレコードを@postに代入
+
+  def index
+    @posts = Post.all.order(id: "DESC")
+  end
+
+	def create
+		Post.create(content: params[:content])
+		redirect_to action: :index
 	end
+	
 end
